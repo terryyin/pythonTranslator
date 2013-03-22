3fc560992ee55ea3ac9b2505c1adfc42bdf1a770
terryyin <terry@odd-e.com>
2013-03-22 13:29:26 +0800

    use regex to match

---
+++ test/dubTest.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  author: terry.yinzhe@gmail.com
+#
+
+import unittest
+from dub import dub_format_exception
+import dub.resource as resource
+
+CODE_SYNTAXERROR_INVALID_SYNTAX = "1+\n"
+NAMEERROR = 'not_defined\n'
+class DubTest(unittest.TestCase):
+    def generatorTraceback(self, code):
+        import sys
+        try:
+            eval(code)
+        except:
+            etype, value, tb = sys.exc_info()
+        traceList = dub_format_exception(etype, value, tb)
+        return traceList
+
+    def testSyntaxError(self):
+        traceList = self.generatorTraceback(CODE_SYNTAXERROR_INVALID_SYNTAX)
+        self.assertIn(resource.ExceptionTypes['SyntaxError']+'\n', traceList)
+    def testIndentationError(self):
+        traceList = self.generatorTraceback(NAMEERROR)
+        self.assertIn(resource.ExceptionTypes['NameError']+'\n', traceList)
+        self.assertNotIn(resource.ExceptionTypes['SyntaxError']+'\n', traceList)
+
+if __name__ == "__main__":
+    unittest.main()
